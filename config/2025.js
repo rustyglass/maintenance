04ae231c64df50b03f25430a0d6c8d747934072f
rustyglass <rusty.glass@icloud.com>
2025-07-12 12:42:39 -0500

    Update 2025.js

--- config/2025.js
+++ config/2025.js
@@ -2,7 +2,7 @@ window.MAINTENANCE_CONFIG = {
   messageHeadline: "We'll Be Right Back",
   messageBody: "Our 2025 Reading Challenge website is currently undergoing scheduled maintenance. Thanks for your patience!",
   contactEmail: "support@readingchallenge.online",
-  logoSrc: "logo.png", // Make sure this matches the file you uploaded
+  logoSrc: "config/2025 Reading Challenge - 512 x 512.png", // Make sure this matches the file you uploaded
 
   // ✅ NEW ETA SUPPORT
   showETA: true,
