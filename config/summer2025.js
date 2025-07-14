6d2a101cee6447b3f4ce82d709e39074e35005e8
rustyglass <rusty.glass@icloud.com>
2025-07-14 12:21:26 -0500

    Update and rename summer2025 to summer2025.js

---
+++ config/summer2025.js
@@ -0,0 +1,20 @@
+window.MAINTENANCE_CONFIG = {
+  messageHeadline: "We'll Be Right Back",
+  messageBody: "Our 2025 Summer Reading Challenge website is currently undergoing scheduled maintenance. Thanks for your patience!",
+  contactEmail: "support@readingchallenge.online",
+  logoSrc: "config/2025 Reading Challenge - 512 x 512.png", // Make sure this matches the file you uploaded
+
+  // ✅ NEW ETA SUPPORT
+  showETA: true,
+  etaTime: "5:00 PM CST",
+
+  statusConfig: {
+    trackedComponents: ["2025 Summer Reading Challenge"],
+    statusPageUrl: "https://readingchallenge.statuspage.io",
+    showOperational: true,
+    showTimestamp: true,
+    hideTimestampOnIncident: true,
+    refreshInterval: 90000,
+    autoHideAfterDays: 2
+  }
+};
