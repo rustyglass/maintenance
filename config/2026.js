46392497549b6d1fc4344e8cd4e3e23e720aa447
rustyglass <rusty.glass@icloud.com>
2025-09-27 14:13:58 -0500

    Create 2026.js

---
+++ config/2026.js
@@ -0,0 +1,20 @@
+window.MAINTENANCE_CONFIG = {
+  messageHeadline: "We'll Be Right Back",
+  messageBody: "Our 2026 Reading Challenge is currently being worked on to finalize some fine details. It will be back online shortly!",
+  contactEmail: "support@readingchallenge.online",
+  logoSrc: "config/2026 Reading Challenge iOS Icon.png", // Make sure this matches the file uploaded
+
+// ETA
+  showETA: true,
+  etaTime: "7:00 PM CST",
+
+  statusConfig: {
+    trackedComponents: ["2026 Reading Challenge"],
+    statusPageUrl: "https://readingchallenge.statuspage.io",
+    showOperational: true,
+    showTimestamp: true,
+    hideTimestampOnIncident: true,
+    refreshInterval: 90000,
+    autoHideAfterDays: 2
+  }
+};
