f4d3307fdf2783b19805d2f6157f76f1c8f14340
rustyglass <rusty.glass@icloud.com>
2025-11-22 20:58:19 -0600

    Update 2025.js

--- config/2025.js
+++ config/2025.js
@@ -1,12 +1,12 @@
 window.MAINTENANCE_CONFIG = {
   messageHeadline: "We'll Be Right Back",
-  messageBody: "Our 2025 Reading Challenge website is currently undergoing scheduled maintenance. Thanks for your patience!",
+  messageBody: "The scheduled maintenance on the 2025 Reading Challenge website has experienced delays and has unveiled design issues at last minute. Thanks for your patience!",
   contactEmail: "support@readingchallenge.online",
   logoSrc: "config/2025 Reading Challenge - 512 x 512.png", // Make sure this matches the file you uploaded
 
   // ✅ NEW ETA SUPPORT
   showETA: true,
-  etaTime: "9:00 PM CST",
+  etaTime: "11-23-2025 by 9:00 PM CST",
 
   statusConfig: {
     trackedComponents: ["2025 Reading Challenge"],
