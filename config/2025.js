b594ddd5ffd3b216f6bb75314eae7c05ccf9fef0
rustyglass <213334447+rustyglass@users.noreply.github.com>
2025-11-27 14:46:58 -0600

    Update 2025.js

--- config/2025.js
+++ config/2025.js
@@ -1,12 +1,12 @@
 window.MAINTENANCE_CONFIG = {
-  messageHeadline: "Website Locked",
-  messageBody: "The 2025 Reading Challenge Website has been locked. Participants will be able to use it at the conclusion of the next Reading Challenge meeting.",
+  messageHeadline: "We'll Be Right Back",
+  messageBody: "Participants cannot use the website during a Reading Challenge meeting.",
   contactEmail: "support@readingchallenge.online",
   logoSrc: "config/2025 Reading Challenge - 512 x 512.png",
 
   // ✅ NEW ETA SUPPORT
-  showETA: false,
-  etaTime: "10:00 PM CST",
+  showETA: true,
+  etaTime: "End of Meeting",
 
   statusConfig: {
     trackedComponents: ["2025 Reading Challenge"],
